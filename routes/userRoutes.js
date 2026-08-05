ce9edc0565cbc7de48b2085c35a73eb33682201f
Tanisha Diyora <tanidiyomix23@gmail.com>
2026-08-05 16:32:23 +0530

    changes in userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import { registerUser } from "../controllers/usercontroller.js";
 
 const router = express.Router();
-
+router.get("/register", (req, res) => {
+    res.send("User Register Route");
+});
 router.post("/register", registerUser);
 
 export default router;
